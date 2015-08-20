d5faccdb3623129d54b6e722ec8f044308b50d26
Ricardo Buquet <ricardo.buquet@groupon.com>
2015-08-20 10:27:51 -0300

    add tests

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,21 @@
 'use strict';
 
-$(function() {
-  console.log('ready')
-});
+var Module = (function () {
+  var showMoreButton = $('#show-more');
+  var moreInformation = $('#more-information');
+
+  showMoreButton.on('click', function() {
+    moreInformation.toggle();
+    if (showMoreButton.opened) {
+      showMoreButton.opened = false;
+      showMoreButton.text('Show More');
+    } else {
+      showMoreButton.opened = true;
+      showMoreButton.text('Show Less');
+    }
+  });
+
+  showMoreButton.opened = false;
+
+  return showMoreButton;
+}());
--- app/test/spec/test.js
+++ app/test/spec/test.js
@@ -1,13 +1,20 @@
 /* global describe, it */
 
 (function () {
-    'use strict';
-
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
-
-            });
-        });
+  'use strict';
+  describe('Windu engineering test', function () {
+    describe('Description', function () {
+      it('should be hidden be default', function () {
+        expect($('#more-information').css('display')).toBe('none');
+      });
+      it('should be visible after one click on button', function () {
+        $('#show-more').click();
+        expect($('#more-information').css('display')).toBe('block');
+      });
+      it('should be hidden after second click on button', function () {
+        $('#show-more').click();
+        expect($('#more-information').css('display')).toBe('none');
+      });
     });
+  });
 })();
